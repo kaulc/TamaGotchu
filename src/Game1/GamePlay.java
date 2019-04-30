51c6423629f9d25cd8866e1dd1ef48aad0b98aa7
Chinar <ckaul@iastate.edu>
2019-04-30 13:38:27 -0500

    removed "press any key to continue"

--- src/Game1/GamePlay.java
+++ src/Game1/GamePlay.java
@@ -6,13 +6,14 @@ import run.MainMenu;
 import thePet.Pet;
 
 public class GamePlay {
-	
+
 	public static Pet myPet;
 
-	//TODO Figure out where to call the menu class after the game is over
-	
-	//FIXME use the passed in Pet to set these values
-	//static Protagonist p = new Protagonist(50, 3, 15, 4000); // STATS WILL BE IMPORTED FROM OTHER PET CLASSES
+	// TODO Figure out where to call the menu class after the game is over
+
+	// FIXME use the passed in Pet to set these values
+	// static Protagonist p = new Protagonist(50, 3, 15, 4000); // STATS WILL BE
+	// IMPORTED FROM OTHER PET CLASSES
 	static Boolean play = true;
 	static Scanner sc = new Scanner(System.in);
 	static Protagonist p;
@@ -25,14 +26,14 @@ public class GamePlay {
 		play = bool;
 	}
 
-	public Protagonist getProtagonist() {
+	public static Protagonist getProtagonist() {
 		return p;
 	}
 
 	public static void setProtagonist(Protagonist pro) {
 		p = pro;
 	}
-	
+
 	public static void newProtagonist(Pet pet1) {
 		p = new Protagonist(pet1.getHealth(), pet1.getLevel(), pet1.getEnergy(), pet1.getPoints());
 		myPet = pet1;
@@ -43,9 +44,9 @@ public class GamePlay {
 	public static void main(String[] args) {
 
 		// Contralar controller = new Contralar();
-		
-		//newProtagonist(myPet);
-		
+
+		// newProtagonist(myPet);
+
 		String tempAns = "";
 		int numSteps = 0; // step = # actions taken to find one monster
 		int numRounds = 0; // round = # monsters fought to reach final monster
@@ -57,7 +58,8 @@ public class GamePlay {
 
 		System.out.println("Welcome to Castle Dash");
 		System.out.println("You find yourself at the entrance of a castle. You read a sign that states: ");
-		System.out.println("- In this castle lies a room of riches. If you find it, you keep it. Enter at your risk.\n");
+		System.out
+				.println("- In this castle lies a room of riches. If you find it, you keep it. Enter at your risk.\n");
 		System.out.println("Press [a] to enter the castle and start playing, [s] for stats, or [d] to exit.");
 
 		if (sc.hasNextLine())
@@ -105,7 +107,7 @@ public class GamePlay {
 
 				while (stepCounter < numSteps && play) {
 					System.out.println();
-					
+
 					if (choice == 1) {
 						System.out.println("You encounter an apple! Pick it up? [d] for yes, [f] for no");
 
@@ -121,8 +123,8 @@ public class GamePlay {
 								System.out.println("Eww.. the apple is way too sour. ");
 								p.addPoints(-10);
 							}
-						}
-						else if (tempAns.equals("f")) System.out.println("You walk away from the apple.");
+						} else if (tempAns.equals("f"))
+							System.out.println("You walk away from the apple.");
 						choice = 4;
 					} else if (choice == 2) {
 						System.out.println("You encounter a heart! <3");
@@ -137,13 +139,11 @@ public class GamePlay {
 							int temp = (int) (Math.random() * 4);
 							if (temp == 0) {
 								break;
-							}
-							else if (temp == 1) {
+							} else if (temp == 1) {
 								System.out.println("<3 It's a chest full of hearts!! <3");
 								p.addHealth(50);
-							}
-							else if (temp == 2) {
-								
+							} else if (temp == 2) {
+
 								System.out.println("It's a chest full of apples :o Eat them? [d] for yes, [f] for no");
 								if (sc.hasNextLine())
 									tempAns = sc.nextLine();
@@ -156,24 +156,22 @@ public class GamePlay {
 										System.out.println("Eww.. the apples are way too sour. ");
 										p.addPoints(-50);
 									}
-								}
-								else if (tempAns.equals("f")) System.out.println("You walk away from the apples.");
-								
-							}
-							else if (temp == 3) {
+								} else if (tempAns.equals("f"))
+									System.out.println("You walk away from the apples.");
+
+							} else if (temp == 3) {
 								System.out.println("It is empty :/");
 							}
-						}
-						else if (tempAns.equals("f")) {
+						} else if (tempAns.equals("f")) {
 							System.out.println("You decide not to open it and walk away.");
 						}
 						choice = 4;
-					}
-					else {
+					} else {
 
 						System.out.print("Now you can either go left, right, or forward. What do you choose? [s/d/f]");
 						System.out.println();
-						if (choice == 10) sc.nextLine();
+						if (choice == 10)
+							sc.nextLine();
 
 						/////
 						if (sc.hasNextLine()) {
@@ -189,7 +187,7 @@ public class GamePlay {
 							choice = p.chooseAction();
 						}
 						/////
-						
+
 					}
 				}
 
@@ -205,26 +203,21 @@ public class GamePlay {
 		System.out.println(
 				"You continue walking forward and reach the end of the hallway, where there is a door that has a sign: ");
 		System.out.println("- Finders keepers ");
-		System.out.println("Press any key to open the door and claim your riches!");
+		// System.out.println("Press any key to open the door and claim your riches!");
 
-		if (sc.hasNextLine()) {
-			System.out.println();
-			System.out.println("You open the door and there lies a room with piles and piles of gold and treasure. "
-					+ "You walk towards the treasures in awe with your arms wide.");
-			System.out.println(
-					"But wait! Suddenly, the door slams shut behind you and you realize you are not alone in the room. You turn around...");
+		System.out.println();
+		System.out.println("You open the door and there lies a room with piles and piles of gold and treasure. "
+				+ "You walk towards the treasures in awe with your arms wide.");
+		System.out.println(
+				"But wait! Suddenly, the door slams shut behind you and you realize you are not alone in the room. You turn around...");
 
-			System.out.println(
-					"\n\nIt's the final monster!! Defeat it to finally claim your riches!");
-			fightMonster(generateBossMonster(p.getLevel()));
-			
-			// generate boss monster and fight him - hard fight
-		}
+		System.out.println("\n\nIt's the final monster!! Defeat it to finally claim your riches!");
+		fightMonster(generateBossMonster(p.getLevel()));
+
+		// generate boss monster and fight him - hard fight
 
 		sc.close();
 	}
-
-	
 
 	private static Monster generateBossMonster(double petlevel) {
 		return new BossMonster(petlevel);
@@ -243,17 +236,18 @@ public class GamePlay {
 	}
 
 	private static Boolean fightMonster(Monster m) {
-		
-		Boolean keypress = false; 
-		
+
+		Boolean keypress = false;
+
 		System.out.println("~~~");
 		int petTurn = (int) (Math.random() * 2); // true or false, randomizes who starts fighting first
 		String tempAns = "";
 
 		m.introStats();
 		keypress = false;
-		System.out.println("Press any key to begin");
-		if (sc.hasNext()) keypress = true;
+		//System.out.println("Press any key to begin");
+		//if (sc.hasNext())
+		//	keypress = true;
 
 		while (p.checkHealth() == true && m.checkHealth() == true) { // fight till someone dies
 			if (petTurn == 1) {
@@ -262,13 +256,13 @@ public class GamePlay {
 
 				tempAns = "x";
 
-				while (!tempAns.equals("s") && !tempAns.equals("d") && !tempAns.equals("f")) { // prompt user for proper input
-					
+				while (!tempAns.equals("s") && !tempAns.equals("d") && !tempAns.equals("f")) { // prompt user for proper
+																								// input
+
 					if (sc.hasNext()) {
 						tempAns = sc.next();
 						tempAns = tempAns.toLowerCase().trim();
 					}
-					
 
 					////
 					if (tempAns.equals("s")) { // CHANGE TO NUMBERS RETURNED FROM TEST BUTTONS
@@ -295,11 +289,9 @@ public class GamePlay {
 					break;
 				petTurn = 0;
 				/*
-				System.out.println("Press any key to continue");
-				keypress = false;
-				sc.next();
-				if (sc.next().equals("p")) keypress = true;
-				*/
+				 * System.out.println("Press any key to continue"); keypress = false; sc.next();
+				 * if (sc.next().equals("p")) keypress = true;
+				 */
 			}
 			if (petTurn == 0) {
 				System.out.println();
@@ -310,12 +302,10 @@ public class GamePlay {
 				petTurn = 1;
 				if (p.checkHealth() == false || m.checkHealth() == false) // if someone dies, break fgfgf
 					break;
-				/*
-				System.out.println("Press any key to continue");
-				keypress = false; 
-				sc.next();
-				if (sc.next().equals("p")) keypress = true;
-				*/
+				/*a
+				 * System.out.println("Press any key to continue"); keypress = false; sc.next();
+				 * if (sc.next().equals("p")) keypress = true;
+				 */
 			}
 
 		}
@@ -329,15 +319,15 @@ public class GamePlay {
 				System.out.println("\nYay, Monster died! You can continue your journey into the castle");
 				p.addPoints(50);
 				play = true;
-			}
-			else if (m.boss) {
+			} else if (m.boss) {
 				System.out.println("\nYay, Boss Monster died! You WIN!!!");
-				play = false; 
-				
+				System.out.println("~~~");
+				play = false;
+
 				MainMenu.menu(myPet);
 				// cut to main menu
 			}
-			
+
 		}
 		System.out.println("~~~");
 		return play;
